89d7935ec9c5dbd61c9cd365643328b50039fff7
LucaDLuffy <LucasRafaeIsaac@gmail.com>
2023-08-28 00:36:13 -0300

    here is a tv stock exercise

---
+++ src/Aplication/Program.java
@@ -0,0 +1,55 @@
+package Aplication;
+
+import Entities.Product;
+import com.sun.management.UnixOperatingSystemMXBean;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter Product Data: ");
+        System.out.print("Name: ");
+        //aqui estou pegando o nome e guardando no product no campo name.
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        //pra fixar eim, pegando o price e guarando no product no campo price usando nextDouble
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in Stock: ");
+        //pra fixar. pegando o quantity e guaradndo no product no campo quantity usando nextInt
+        product.quantity = sc.nextInt();
+
+        //só pra pular linha mesmo
+        System.out.println();
+        //usando o toString parar retorna oque eu alterei, mas posso usar só o productg desde que adicione
+        //o toString na minha classe, vou deixar parar fixar melhor.
+        System.out.print("Product Data: " + product.toString());
+
+        System.out.println();
+        //aqui estamos adicionado o produto
+        System.out.print("Enter the number of Products to be add in Stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        //aqui só usei product mas como mudei com o to string já vai
+        System.out.print("Updating Data: " + product);
+
+        System.out.println();
+        //aqui estamos removendo os produtos
+        System.out.print("Enter the number of Products to be Remove in Stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.print("Updating Data: " + product);
+
+
+
+
+
+        sc.close();
+    }
+}
---
+++ src/Entities/Product.java
@@ -0,0 +1,30 @@
+package Entities;
+
+public class Product {
+
+    public String name;
+    public double price;
+    //quantity atributo da classe
+    public int quantity;
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        //usando this. para usar o quantity do atributo e não o parametro
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        //aqui usando novamente o this.
+        this.quantity -= quantity;
+    }
+
+    //Aqui estou reescrevendo o String padrão e aplicando o metodo da minha escolha para faciliar o processo.
+    //também usei uma mascara de formatação para deixa com duas casas decimais %.2f.
+    public String toString(){
+        return name + ", $ " + String.format("%.2f", price) + ", " + quantity +
+                " units, total: $ " +  String.format("%.2f", totalValueInStock());
+    }
+}
